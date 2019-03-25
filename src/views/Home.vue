cd58ced579e8a3ef8ebf99b18f8b238034706bed
Geddy <g3ddylee@gmail.com>
2019-03-24 23:00:00 -0400

    Simplify

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,27 +1,26 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png"><br>
-    <a v-if="!user.token" href="/auth/spotify">Sign in With Spotify</a>
-    <div v-if="user.token">Welcome back!</div>
+    <a v-if="!token" href="/auth/spotify">Sign in With Spotify</a>
+    <div v-if="token">Welcome back!</div>
   </div>
 </template>
 
 <script>
-import { mapState, mapMutations } from 'vuex'
+import { mapMutations } from 'vuex'
 
 export default {
   name: 'home',
+  data () {
+    return {
+      token: null
+    }
+  },
   methods: {
     ...mapMutations(['setUser'])
   },
-  computed: {
-    ...mapState(['user'])
-  },
-  beforeMount () {
-    const token = this.$cookies.get('user.token')
-    if (token) {
-      this.setUser(token)
-    }
+  mounted () {
+    this.token = this.$cookies.get('user.token')
   }
 }
 </script>
