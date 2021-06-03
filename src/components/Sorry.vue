9d75847f69b4ce14181aea88dfb4718e338029c6
Christopher Lee <christopher.lee@warnerbros.com>
2021-06-02 22:37:19 -0400

    feat: site down warning

---
+++ src/components/Sorry.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <p class="msg">
+      Sorry for the inconvenience! It looks like our Lyrics provider has pulled
+      the plug on our operation. Don't worry though - we will be back as soon as
+      possible. In other words, as soon as I can figure something out. Please
+      keep us bookmarked and check back soon.
+    </p>
+    <p class="msg">
+      SpotifyKaraoke started as a service built for one person, and receives far
+      more support than I imagined it would, so I am working to get it back up
+      and running. Thank you for all of your support!
+    </p>
+    <p class="msg">
+      Geddy
+    </p>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.msg {
+  max-width: 80vw;
+  font-size: 24px;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="home">
-    <TopMenu />
-    <div v-if="!song && notListening" class="home__not-listening">
+    <Sorry />
+    <!-- <TopMenu /> -->
+    <!-- <div v-if="!song && notListening" class="home__not-listening">
       You're signed in to Spotify, but not listening to anything.
       <br />
       <br />Start
@@ -27,12 +28,14 @@
       class="home__lyrics">
       <Karaoke />
       <ProgressBar />
-    </div>
+    </div> -->
   </div>
 </template>
 
 <script>
+/* eslint-disable vue/no-unused-components */
 import TopMenu from '@/components/TopMenu.vue';
+import Sorry from '@/components/Sorry.vue';
 import Karaoke from '@/components/Karaoke.vue';
 import ProgressBar from '@/components/ProgressBar.vue';
 import { mapMutations, mapState, mapActions } from 'vuex';
@@ -42,7 +45,8 @@ export default {
   components: {
     TopMenu,
     Karaoke,
-    ProgressBar
+    ProgressBar,
+    Sorry
   },
   data() {
     return {
@@ -126,53 +130,53 @@ export default {
     if (!this.isLoggedIn) {
       this.$router.push({ path: 'login' });
     }
-    this.startInterval(2000);
+    // this.startInterval(2000);  //re-enable me!
   },
   beforeDestroy() {
     window.clearTimeout(this.queryTimer);
   },
-  watch: {
-    song: {
-      immediate: true,
-      handler: async function(value) {
-        if (!value) {
-          this.notListening = true;
-          this.startInterval(2000);
-          this.fetchingLyrics = false;
-        } else if (
-          this.idleMode ||
-          (
-            value.artist &&
-            value.trackName &&
-            (value.artist !== this.artist || value.trackName !== this.trackName)
-          )
-        ) {
-          // Reset all the idle counters.
-          this.idleMode = false;
-          this.idleCounter = 0;
-          this.startInterval(2000);
+  // watch: {
+  //   song: {
+  //     immediate: true,
+  //     handler: async function(value) {
+  //       if (!value) {
+  //         this.notListening = true;
+  //         this.startInterval(2000);
+  //         this.fetchingLyrics = false;
+  //       } else if (
+  //         this.idleMode ||
+  //         (
+  //           value.artist &&
+  //           value.trackName &&
+  //           (value.artist !== this.artist || value.trackName !== this.trackName)
+  //         )
+  //       ) {
+  //         // Reset all the idle counters.
+  //         this.idleMode = false;
+  //         this.idleCounter = 0;
+  //         this.startInterval(2000);
 
-          this.notListening = false;
+  //         this.notListening = false;
 
-          this.artist = value.artist;
-          this.trackName = value.trackName;
+  //         this.artist = value.artist;
+  //         this.trackName = value.trackName;
 
-          this.fetchingLyrics = true;
-          let trackName = this.song.trackName;
+  //         this.fetchingLyrics = true;
+  //         let trackName = this.song.trackName;
 
-          trackName = trackName.replace(/remaster(?:ed)? \d{2,4}/i, '');
-          trackName = trackName.replace(/\d{2,4} remaster(?:ed)?/i, '');
-          trackName = trackName.replace(/remaster(?:ed)?/i, '');
+  //         trackName = trackName.replace(/remaster(?:ed)? \d{2,4}/i, '');
+  //         trackName = trackName.replace(/\d{2,4} remaster(?:ed)?/i, '');
+  //         trackName = trackName.replace(/remaster(?:ed)?/i, '');
 
-          this.getLyrics({
-            query: `${this.song.artist} ${trackName}`
-          }).then(() => {
-            this.fetchingLyrics = false;
-          });
-        }
-      }
-    }
-  }
+  //         this.getLyrics({
+  //           query: `${this.song.artist} ${trackName}`
+  //         }).then(() => {
+  //           this.fetchingLyrics = false;
+  //         });
+  //       }
+  //     }
+  //   }
+  // }
 };
 </script>
 
